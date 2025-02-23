01036bd6c98b0b4c893284186167d09828cb8f2a
juancrfig <juancrfig@gmail.com>
2025-02-23 16:14:09 -0500

    My first VS alert!

--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,6 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autoquery" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -19,6 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello VS Code!');
 	});
 
+	const myAlert = vscode.commands.registerCommand('autoquery.myAlert', () => {
+		vscode.window.showInformationMessage('C H A M O');
+	});
+
+	context.subscriptions.push(myAlert);
 	context.subscriptions.push(disposable);
 }
 
