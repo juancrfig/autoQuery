ffb66d131725c56d698ad33e381cb856c0c4528d
juancrfig <juancrfig@gmail.com>
2025-02-23 18:45:17 -0500

    Register my first key shortcut

--- src/extension.ts
+++ src/extension.ts
@@ -9,18 +9,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('autoquery.helloWorld', () => {
+	const welcomeMessage = vscode.commands.registerCommand('autoquery.welcomeMessage', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
+		vscode.window.showInformationMessage('Welcome to autoQuery!');
 	});
 
-	const myAlert = vscode.commands.registerCommand('autoquery.myAlert', () => {
-		vscode.window.showInformationMessage('C H A M O');
+	const queryCommand = vscode.commands.registerCommand('autoquery.runQuery', () => {
+		
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor found!');
+			return;
+		}
+		vscode.window.showInformationMessage('An active editor has been detected!');
 	});
 
-	context.subscriptions.push(myAlert);
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(welcomeMessage);
+	context.subscriptions.push(queryCommand);
+
+
 }
 
 // This method is called when your extension is deactivated
